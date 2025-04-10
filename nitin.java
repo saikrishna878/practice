6eeeda9f0b9418b4f9caad8e4d33cfc7acb5e6b5
saikrishna878 <nanaboinasaikrishna555@gmail.com>
2025-04-10 09:36:22 +0530

    Update nitin.java

--- nitin.java
+++ nitin.java
@@ -1,5 +1,5 @@
 public class nitin{
       public static void main(String args[]){
-      System.out.println("Hey F NITIN");
+      System.out.println("Hey F NITI      check in  N");
       }
       }
